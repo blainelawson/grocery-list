c0aa7474c30c3b226f77ea3001627ec105418cf2
Blaine Lawson <blaine.lawson@gmail.com>
2020-01-10 14:04:36 -0500

    Built simple grocery list app.

--- grocery-list/Program.cs
+++ grocery-list/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace grocery_list
 {
@@ -6,7 +7,52 @@ namespace grocery_list
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            string item;
+
+            List<string> groceryList = new List<string>();
+
+            Console.WriteLine("Grocery List");
+
+            do
+            {
+                Console.Write("Add/Remove/Display items from grocery list (Add/Remove/Display/Exit): ");
+                input = Console.ReadLine();
+
+                switch(input)
+                {
+                    case "Add":
+                        Console.Write("What would you like to add? ");
+                        item = Console.ReadLine();
+                        groceryList.Add(item);
+                        Console.WriteLine(item + " has been added.");
+                        break;
+                    case "Remove":
+                        Console.Write("What would you like to remove? ");
+                        item = Console.ReadLine();
+                        groceryList.Remove(item);
+                        Console.WriteLine(item + " has been removed.");
+                        break;
+                    case "Display":
+                        DisplayGroceries(groceryList);
+                        break;
+                    case "Exit":
+                        break;
+                    default:
+                        Console.WriteLine(input + " is not a valid choice");
+                        break;
+                }
+
+            } while (input != "Exit");
+            
+        }
+
+        static void DisplayGroceries(List<string> groceryList)
+        {
+            foreach (string item in groceryList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
