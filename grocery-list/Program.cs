2cadba3722e68a3ab400ab6b156b8204040c7054
Blaine Lawson <blaine.lawson@gmail.com>
2020-01-10 13:37:11 -0500

    Add project files.

---
+++ grocery-list/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grocery_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
